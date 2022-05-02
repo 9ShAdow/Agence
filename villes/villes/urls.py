aaafb5f27d1fb6848f622dea62cc21f40858aa35
9ShAdow <95073328+9ShAdow@users.noreply.github.com>
2022-05-02 14:26:36 +0200

    V2 + pp

--- villes/villes/urls.py
+++ villes/villes/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('',include('visite.urls')),
+    path('visite/', include('visite.urls')),
     path('admin/', admin.site.urls),
-    path('visite/', include('visite.urls'))
 ]
